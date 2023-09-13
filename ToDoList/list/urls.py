b11378d549ca97df3c0b2917ce036c1186b25a11
BEAUTIFULUU <139879152+BEAUTIFULUU@users.noreply.github.com>
2023-09-13 17:50:30 +0200

    Delete ToDoList directory

--- ToDoList/list/urls.py
+++
@@ -1,9 +0,0 @@
-from rest_framework_nested import routers
-from django.urls import path, include
-from . import views
-from .views import TasksViewSet
-
-router = routers.DefaultRouter()
-router.register('tasks', views.TasksViewSet, basename='tasks')
-
-urlpatterns = router.urls
